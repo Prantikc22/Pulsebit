a7aa1b8f3331e5e56a74e3bcc12c9a48bab47273
prantikc22 <prantikchatterjee22@gmail.com>
2025-07-27 16:47:25 +0530

    Initial commit: Pulsebit rebranded from Umami

---
+++ src/app/share/[...shareId]/Header.tsx
@@ -0,0 +1,29 @@
+import { Icon, Text } from 'react-basics';
+import Link from 'next/link';
+import LanguageButton from '@/components/input/LanguageButton';
+import ThemeButton from '@/components/input/ThemeButton';
+import SettingsButton from '@/components/input/SettingsButton';
+import Icons from '@/components/icons';
+import styles from './Header.module.css';
+
+export function Header() {
+  return (
+    <header className={styles.header}>
+      <div>
+        <Link href="https://pulsebit.com" target="_blank" className={styles.title}>
+          <Icon size="lg">
+            <Icons.Logo />
+          </Icon>
+          <Text>Pulsebit</Text>
+        </Link>
+      </div>
+      <div className={styles.buttons}>
+        <ThemeButton />
+        <LanguageButton />
+        <SettingsButton />
+      </div>
+    </header>
+  );
+}
+
+export default Header;
